8b4720d6f56451ebe66a196b49a37e11f33f2f75
nib <139529654+whiteNib@users.noreply.github.com>
2023-10-11 16:50:12 +0900

    프로젝트 상세 페이지 브랜치
    프로젝트 상세 페이지 브랜치
    상품이 여러개일 때 상황 체크 필요

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import './components/ProjectPage.css'
+//import ProjectData from './components/ProjectData';
+import ProjectData2 from './components/ProjectData2';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>최종 프로젝트 팀6</h1>
+      <h1>json 데이터 가져오기(프로젝트 상세 페이지)</h1>
+      <hr />헤더영역<hr />
+
+      {/* <ProjectData></ProjectData> */}
+      <ProjectData2></ProjectData2>
     </div>
   );
 }
