4c4c6c238738094610042eb8246baf9eec7cf403
nib <139529654+whiteNib@users.noreply.github.com>
2023-10-11 18:12:08 +0900

    Update ProjectData2.jsx

--- src/components/ProjectData2.jsx
+++ src/components/ProjectData2.jsx
@@ -3,7 +3,7 @@ import useFetch from '../components/hooks/useFetch';
 
 //console.log(apiUrl);
 
-function ProjectData() {
+function ProjectData2() {
 
     const ProjectData = useFetch("https://json-server-vercel-sepia-omega.vercel.app/projects"); //api
     if (ProjectData !== undefined && ProjectData !== null && ProjectData.length) { // 데이터가 언디파인이 아니거나 존재하거나 데이터가 조회되면
@@ -16,23 +16,35 @@ function ProjectData() {
             {ProjectData !== undefined && ProjectData !== null && ProjectData.length > 0 &&
                 ProjectData.map((item) => (
                     <div>
-                        <h1 className='center'>{item.projName}</h1>
+                        <h1>{item.projName}</h1>
                         <div className='center'>
                             <div id='div1'><img id='mainImage' src={item.projMainImgPath} alt='메인 사진'></img></div>
                             <div id='div2'>
-                                <div key={item.projReward}>
-                                    마감일 : {item.projReward[0].projRewardName}
-                                    <br />
-                                    <br />
-                                    마감일 : {item.projReward[1].projRewardName}
-                                    <br />
-                                    <br />
-                                    리워드 금액 : {item.projReward[0].projRewardAmount}원
-                                    <br />
-                                    <br />
-                                    리워드 갯수 : {item.projReward[0].projRewardCount}개
-                                    <br />
-                                    <br />
+                                {item.projReward.map((reward) => (
+                                    <div>
+                                        <select className='select' name="">
+
+                                            <option>
+                                                마감일 : {reward.projRewardName}
+                                                <br />
+                                                리워드 금액 : {reward.projRewardAmount}원
+                                                <br />
+                                                리워드 갯수 : {reward.projRewardCount}개
+                                                <br />
+                                            </option>
+                                        </select>
+                                        마감일 : {reward.projRewardName}
+                                        <br />
+                                        <br />
+                                        리워드 금액 : {reward.projRewardAmount}원
+                                        <br />
+                                        <br />
+                                        리워드 갯수 : {reward.projRewardCount}개
+                                        <br />
+                                        <br />
+                                    </div>
+                                ))}
+                                <div>
                                     펀딩 시작일 : {item.projFundDate[0].projFundStartDate}
                                     <br />
                                     <br />
@@ -72,15 +84,15 @@ function ProjectData() {
                         {item.projDesc}
                         <br />
                         <hr /> */}
-                        <div id='projDesc'>
+                        <div div id='projDesc' >
                             <div dangerouslySetInnerHTML={{ __html: item.projDesc }} />
                         </div>
-                    </div>
+                    </div >
                 ))
             }
 
-        </div>
+        </div >
     );
 }
 
-export default ProjectData
+export default ProjectData2
