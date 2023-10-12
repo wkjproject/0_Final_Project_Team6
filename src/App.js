4bdcda4c8690f76c457773f06c5f7c500f4d7d14
Guchaehyun <Gucheahyun@gmail.com>
2023-10-12 15:15:15 +0900

    font및 헤더 위치 일부 조정
    글꼴 사이즈 다양화, 헤더 위치 일부 조정.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import './components/ProjectPage.css'
+import "./App.css";
+import "./components/ProjectPage.css";
 //import ProjectData from './components/ProjectData';
-import ProjectData2 from './components/ProjectData2';
+//import ProjectData2 from './components/ProjectData2';
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from './components/Header/Header';
-import Home from './components/Pages/Home';
-import OpenProj from './components/Pages/OpenProj';
-import NewProj from './components/Pages/NewProj';
-import DeadlineProj from './components/Pages/DeadlineProj';
-import SearchPage from './components/Pages/SearchPage';
-import Login from './components/Pages/Login';
-import Signup from './components/Pages/Signup';
-import NotFound from './components/Pages/NotFound';
-import LandingPage from './components/LandingPage/LandingPage';
-
+import Header from "./components/Header/Header";
+import Home from "./components/Pages/Home";
+import OpenProj from "./components/Pages/OpenProj";
+import NewProj from "./components/Pages/NewProj";
+import DeadlineProj from "./components/Pages/DeadlineProj";
+import SearchPage from "./components/Pages/SearchPage";
+import Login from "./components/Pages/Login";
+import Signup from "./components/Pages/Signup";
+import NotFound from "./components/Pages/NotFound";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
