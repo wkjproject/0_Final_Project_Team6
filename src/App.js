a99d05e55f965a9016645897a8c864866795d1b4
nib <139529654+whiteNib@users.noreply.github.com>
2023-10-12 14:07:10 +0900

    project data 도메인 추가
    project data 도메인 추가

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/*" element={<NotFound />} />
+          <Route path="/project2" element={<ProjectData2 />} />
         </Routes>
       </BrowserRouter>
       {/* <hr /> */}
