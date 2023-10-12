97c192c579bb400e24ed189c7c7b11def1695935
jwk <nakazjk@naver.com>
2023-10-12 13:52:05 +0900

    pull origin main

--- src/App.js
+++ src/App.js
@@ -10,26 +10,33 @@ import OpenProj from './components/Pages/OpenProj';
 import NewProj from './components/Pages/NewProj';
 import DeadlineProj from './components/Pages/DeadlineProj';
 import SearchPage from './components/Pages/SearchPage';
-import Login from './components/Pages/Login';
 import Signup from './components/Pages/Signup';
 import NotFound from './components/Pages/NotFound';
 import LandingPage from './components/LandingPage/LandingPage';
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './components/Pages/Home';
-import OpenProj from './components/Pages/OpenProj';
-import NewProj from './components/Pages/NewProj';
-import DeadlineProj from './components/Pages/DeadlineProj';
 import { Login } from './components/Pages/MemberService/Login';
-import Signup from './components/Pages/MemberService/Signup';
-import NotFound from './components/Pages/NotFound';
 import IdpwFind from './components/Pages/MemberService/IdpwFind';
 
 function App() {
   return (
     <div className='App'>
-      <h1>최종 프로젝트 팀6</h1>
+      {/* <ProjectData></ProjectData> */}
+
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/openProj' element={<OpenProj />} />
+          <Route path='/newProj' element={<NewProj />} />
+          <Route path='/deadlineProj' element={<DeadlineProj />} />
+          <Route path='/searchPage' element={<SearchPage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/IdpwFind' element={<IdpwFind />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <ProjectData2></ProjectData2>
     </div>
   );
 }
