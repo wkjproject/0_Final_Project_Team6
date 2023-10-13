e17bef4bc67f55cb0219a4462bf8f79bc6d14a52
nib <139529654+whiteNib@users.noreply.github.com>
2023-10-13 16:43:26 +0900

    로컬 컴퓨터에서 파일 복사
    로컬 컴퓨터에서 파일 복사

---
+++ src/components/MenuTabs.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import useFetch from '../components/hooks/useFetch';
+
+class MenuTabs extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            activeTab: 'tab1', // 초기 활성 탭 설정
+        };
+    }
+
+    // 메뉴 항목을 클릭할 때 호출되는 핸들러
+    handleTabClick = (tab) => {
+        this.setState({ activeTab: tab });
+    };
+
+    render() {
+        return (
+            <div>
+                <ul className="tab-list">
+                    <li onClick={() => this.handleTabClick('tab1')}>소개글</li>
+                    <li onClick={() => this.handleTabClick('tab2')}>코멘트</li>
+                    <li onClick={() => this.handleTabClick('tab3')}>QnA</li>
+                    <li onClick={() => this.handleTabClick('tab4')}>회사 소개</li>
+                </ul>
+                <div className="tab-content">
+                    {this.state.activeTab === 'tab1' && <Tab1Content />}
+                    {this.state.activeTab === 'tab2' && <Tab2Content />}
+                    {this.state.activeTab === 'tab3' && <Tab3Content />}
+                    {this.state.activeTab === 'tab4' && <Tab4Content />}
+                </div>
+            </div>
+        );
+    }
+}
+
+// 각 탭에 대한 컴포넌트
+function Tab1Content() {
+
+    const ProjectData = useFetch("https://json-server-vercel-sepia-omega.vercel.app/projects"); //api
+    if (ProjectData !== undefined && ProjectData !== null && ProjectData.length) { // 데이터가 언디파인이 아니거나 존재하거나 데이터가 조회되면
+    }
+
+    return (
+        <div>
+            {ProjectData !== undefined && ProjectData !== null && ProjectData.length > 0 &&
+                ProjectData.map((item) => (
+                    <div dangerouslySetInnerHTML={{ __html: item.projDesc }} />
+                ))}
+        </div>
+
+    );
+}
+
+function Tab2Content() {
+    return (
+        <div>코멘트 페이지 추가 예정</div>
+    );
+}
+
+function Tab3Content() {
+    return (
+        <div>QnA 페이지 추가 예정</div>
+    );
+}
+
+function Tab4Content() {
+    return (
+        <div>회사소개 페이지 추가 예정</div>
+    );
+}
+
+export default MenuTabs;
---
+++ src/components/RewardSelect.js
@@ -0,0 +1,175 @@
+// 상품 리워드 전체 목록을 셀렉트 형식으로 볼 수 있는 컴포넌트
+// 선택한 리워드는 바로 아래에 추가되고, 중복 선택해도 추가되지 않음
+// x로 선택한 리워드를 제거가능, 선택한 리워드의 금액은 하단에 계산됨
+// 에러 없음
+
+import React, { useState, useRef, useEffect } from 'react';
+import useFetch from './hooks/useFetch';
+import './Select.css'
+
+const RewardSelect = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedRewards, setSelectedRewards] = useState([]);
+  const [heartClicked, setHeartClicked] = useState(false);
+  const [clickedCount, setClickedCount] = useState(0);
+
+
+  const toggleHeart = () => {
+    if (heartClicked) {
+      setHeartClicked(false);
+      setClickedCount(clickedCount - 1);
+    } else {
+      setHeartClicked(true);
+      setClickedCount(clickedCount + 1);
+    }
+  };
+
+  const ProjectData = useFetch(
+    'https://json-server-vercel-sepia-omega.vercel.app/projects'
+  );
+
+  const modalRef = useRef(null);
+  const backgroundAreaRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        backgroundAreaRef.current &&
+        !backgroundAreaRef.current.contains(event.target)
+      ) {
+        setShowModal(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const formatAmount = (amount) => {
+    return amount.toLocaleString();
+  };
+
+  const calculateTotalAmount = () => {
+    return selectedRewards.reduce((total, reward) => total + reward.projRewardAmount, 0);
+  };
+
+  const handleRewardSelect = (reward) => {
+    if (!selectedRewards.some((r) => r.projRewardName === reward.projRewardName)) {
+      setSelectedRewards([...selectedRewards, reward]);
+    }
+    setShowModal(false); // 리스트가 선택되면 모달 닫기
+  };
+
+  const handleRewardRemove = (rewardToRemove) => {
+    const updatedRewards = selectedRewards.filter(
+      (reward) => reward !== rewardToRemove
+    );
+    setSelectedRewards(updatedRewards);
+  };
+
+  return (
+    <div className="backgroundArea" ref={backgroundAreaRef}>
+      <button onClick={() => setShowModal(!showModal)} className="reward-button">
+        <span className="reward-text">
+          {showModal ? '리워드를 선택해주세요.' : '리워드 선택하기'}
+        </span>{" "}
+        <span className="arrow">{showModal ? '▲' : '▼'}</span>
+      </button>
+      {showModal && (
+        <div className="modal" ref={modalRef} style={{ width: '350px' }}>
+          <div className="modal-content">
+            <ul className="no-bullets">
+              {ProjectData.map((project, projectIndex) => (
+                <li key={project.proj_id}>
+                  {project.projReward.map((reward, index) => (
+                    <div key={index}>
+                      <button
+                        onClick={() => handleRewardSelect(reward)}
+                        className="selectButton"
+                      >
+                        <table>
+                          <tr>
+                            <td>리워드</td>
+                            <td>: {reward.projRewardName}</td>
+                          </tr>
+                          <tr>
+                            <td>금액</td>
+                            <td>: {formatAmount(reward.projRewardAmount)}</td>
+                          </tr>
+                          <tr>
+                            <td>갯수</td>
+                            <td>: {reward.projRewardCount}</td>
+                          </tr>
+                        </table>
+                      </button>
+                      {projectIndex !== ProjectData.length - 1 || index !== project.projReward.length - 1 ? <hr /> : null}
+                    </div>
+                  ))}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {selectedRewards.length > 0 && (
+        <div>
+          <h2 className="selected-reward-header">선택한 리워드 :</h2>
+          <ul className="no-bullets">
+            {selectedRewards.map((selectedReward, index) => (
+              <li key={index} className={index >= 0 && index <= 100 ? "second-table" : ""}>
+                <table>
+                  <tr>
+                    <td>
+                      리워드
+                    </td>
+                    <td>
+                      : {selectedReward.projRewardName}
+                    </td>
+                    <td className='button-cell'>
+                      <button
+                        className="remove-button"
+                        onClick={() => handleRewardRemove(selectedReward)}
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>금액</td>
+                    <td>: {formatAmount(selectedReward.projRewardAmount)} 원</td>
+                  </tr>
+                  <tr>
+                    <td>갯수</td>
+                    <td>: {selectedReward.projRewardCount}</td>
+                  </tr>
+
+                </table>
+                {index !== selectedRewards.length - 1 ? <hr /> : null}
+              </li>
+            ))}
+          </ul>
+          <div className="total-amount">
+            <h3>총 금액 : {formatAmount(calculateTotalAmount())} 원</h3>
+          </div>
+        </div>
+      )}
+
+      <div className="button-container">
+        <button className="fundingBtn">신청하기</button>
+        <div className="button-group">
+          <button className={`heartBtn ${heartClicked ? 'clicked' : ''}`} onClick={toggleHeart}>
+            {heartClicked ? '❤️' : '🤍'} {heartClicked ? clickedCount : ''}
+          </button>
+          <button className="shareBtn" style={{ marginLeft: '20px' }}>공유하기</button>
+        </div>
+      </div>
+    </div>
+  );
+
+};
+
+export default RewardSelect;
