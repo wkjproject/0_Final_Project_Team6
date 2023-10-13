3cfc8e792c706fb885c78e86ea36b439cffc60c2
sahn00 <53425263+sahn00@users.noreply.github.com>
2023-10-13 13:19:01 +0900

    채현님 가이드로 프로젝트카드 스타일 조정

--- src/components/Pages/ProjectCard.jsx
+++ src/components/Pages/ProjectCard.jsx
@@ -6,11 +6,9 @@ export default function ProjectCard({image, location, title, price, dday, isNew}
   return (
   <div className="project-card">
     <Thumbnail image={image} isNew={isNew}/>
-    <h3>{title}</h3>
-    <p>{location}</p>
-    <p>{price}</p>
-    <p>{dday}</p>
-
+    <h5 className='pc-title'>{title}</h5>
+    <p className='pc-location'>{location} | <span className='pc-price'>{price}원</span></p>
+    
   </div>
   )
 }
--- src/components/Pages/ProjectsList.jsx
+++ src/components/Pages/ProjectsList.jsx
@@ -32,21 +32,21 @@ function ProjectList() {
         title='선데이 어드벤쳐 보드게임'
         location='춘천시'
         dday='09월 24일(일)'
-        price='5,000원~'
+        price='5,000'
         isNew={true}
       />
       <ProjectCard 
         image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
         title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
         location='부산'
-        price='11,000원~'
+        price='11,000'
         isNew={true}
         />
       <ProjectCard
         image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
         title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
         location='부산'
-        price='11,000원'
+        price='11,000'
         isNew={true}
       />
       <ProjectCard 
@@ -54,21 +54,21 @@ function ProjectList() {
         title='선데이 어드벤쳐 보드게임'
         location='춘천시'
         dday='09월 24일(일)'
-        price='5,000원~'
+        price='5,000'
         isNew={true}
       />
       <ProjectCard 
         image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
         title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
         location='부산'
-        price='11,000원~'
+        price='11,000'
         isNew={true}
         />
       <ProjectCard
         image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
         title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
         location='부산'
-        price='11,000원'
+        price='11,000'
         isNew={true}
       />
       <ProjectCard 
@@ -76,21 +76,21 @@ function ProjectList() {
         title='선데이 어드벤쳐 보드게임'
         location='춘천시'
         dday='09월 24일(일)'
-        price='5,000원~'
+        price='5,000'
         isNew={true}
       />
       <ProjectCard 
         image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
         title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
         location='부산'
-        price='11,000원~'
+        price='11,000'
         isNew={true}
         />
       <ProjectCard
         image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
         title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
         location='부산'
-        price='11,000원'
+        price='11,000'
         isNew={true}
       />
       <ProjectCard 
@@ -98,21 +98,21 @@ function ProjectList() {
         title='선데이 어드벤쳐 보드게임'
         location='춘천시'
         dday='09월 24일(일)'
-        price='5,000원~'
+        price='5,000'
         isNew={true}
       />
       <ProjectCard 
         image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
         title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
         location='부산'
-        price='11,000원~'
+        price='11,000'
         isNew={true}
         />
       <ProjectCard
         image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
         title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
         location='부산'
-        price='11,000원'
+        price='11,000'
         isNew={true}
       />
     </div>
