f1bf9c7c60458f924b819939cf4e171f45fd2ac9
sahn00 <53425263+sahn00@users.noreply.github.com>
2023-10-13 11:26:47 +0900

    플젝카드 임시커밋

--- src/components/Pages/ProjectsList.jsx
+++ src/components/Pages/ProjectsList.jsx
@@ -6,12 +6,27 @@ import useFetch from '../hooks/useFetch';
 function ProjectList() {
   const ProjectData = useFetch("/projects");
   if (ProjectData !== undefined && ProjectData !== null && ProjectData.length) { // 데이터가 언디파인이 아니거나 존재하거나 데이터가 조회되면
-    console.log(ProjectData);
+    console.log("프로젝트데이터:", ProjectData);
     //console.log(ProjectData[0]);
-}
+  }
 
   return (
     <div className="project-list">
+      {ProjectData !== undefined && ProjectData !== null && ProjectData.length > 0 &&
+        ProjectData.map((proj) => (
+          <ProjectCard 
+            key={proj.projName}
+            image={proj.projMainImgPath}
+            title={proj.projName}
+            location={proj.projAddr.split(' ',2)[1]}
+
+            dday={proj.projDate}
+            price={proj.projReward[0].projRewardAmount}
+            isNew={true}
+        />
+        ))
+      }
+      <hr></hr>
       <ProjectCard 
         image='https://eventusstorage.blob.core.windows.net/evs/Image/kyrielle/71287/ProjectInfo/Cover/1a6c262bb3664008b6475814bac58626.jpg'
         title='선데이 어드벤쳐 보드게임'
--- src/components/Pages/Thumbnail.jsx
+++ src/components/Pages/Thumbnail.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 
 export default function Thumbnail ({image, isNew}) {
-  console.log('Thumbnail image: ', image);
-  console.log('Thumbnail isNew: ', isNew);
+  //console.log('Thumbnail image: ', image);
+  //console.log('Thumbnail isNew: ', isNew);
   return (
     <div className="thumbnail">
-      {isNew && <span className='new-tag'>new</span>}
       <img 
         className="photo"
         src={image}
-        alt='thumbnail'
-      />
+        alt=''
+        />
+      {isNew && <span className='new-tag'>new</span>}
     </div>
   );
 }
