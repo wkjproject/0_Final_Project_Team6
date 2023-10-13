847d1b6ea02e5d49776d8bd585c688387a94979c
sahn00 <53425263+sahn00@users.noreply.github.com>
2023-10-13 17:22:02 +0900

    css 수정

--- src/components/Pages/Thumbnail.jsx
+++ src/components/Pages/Thumbnail.jsx
@@ -1,10 +1,17 @@
 import React from "react"
+import { useNavigate } from 'react-router';
 
 export default function Thumbnail ({image, isNew}) {
   //console.log('Thumbnail image: ', image);
   //console.log('Thumbnail isNew: ', isNew);
+
+  const navigate = useNavigate();
+  const openProjDetails = () => {
+    navigate("/project2");
+  }
+
   return (
-    <div className="thumbnail">
+    <div className="thumbnail"  onClick={openProjDetails}>
       <img 
         className="photo"
         src={image}
