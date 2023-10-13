ad85dbca4f9c006295aa51793fc9917f960da343
sahn00 <53425263+sahn00@users.noreply.github.com>
2023-10-13 15:16:31 +0900

    프로젝트 리스트 표시 스타일 수정

--- src/components/Pages/ProjectsList.jsx
+++ src/components/Pages/ProjectsList.jsx
@@ -26,95 +26,25 @@ function ProjectList() {
         />
         ))
       }
-      <hr></hr>
-      <ProjectCard 
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/kyrielle/71287/ProjectInfo/Cover/1a6c262bb3664008b6475814bac58626.jpg'
-        title='선데이 어드벤쳐 보드게임'
-        location='춘천시'
-        dday='09월 24일(일)'
-        price='5,000'
-        isNew={true}
-      />
-      <ProjectCard 
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
-        title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
-        location='부산'
-        price='11,000'
-        isNew={true}
-        />
-      <ProjectCard
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
-        title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
-        location='부산'
-        price='11,000'
-        isNew={true}
-      />
-      <ProjectCard 
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/kyrielle/71287/ProjectInfo/Cover/1a6c262bb3664008b6475814bac58626.jpg'
-        title='선데이 어드벤쳐 보드게임'
-        location='춘천시'
-        dday='09월 24일(일)'
-        price='5,000'
-        isNew={true}
-      />
-      <ProjectCard 
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
-        title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
-        location='부산'
-        price='11,000'
-        isNew={true}
-        />
-      <ProjectCard
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
-        title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
-        location='부산'
-        price='11,000'
-        isNew={true}
-      />
-      <ProjectCard 
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/kyrielle/71287/ProjectInfo/Cover/1a6c262bb3664008b6475814bac58626.jpg'
-        title='선데이 어드벤쳐 보드게임'
-        location='춘천시'
-        dday='09월 24일(일)'
-        price='5,000'
-        isNew={true}
-      />
-      <ProjectCard 
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
-        title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
-        location='부산'
-        price='11,000'
-        isNew={true}
-        />
-      <ProjectCard
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
-        title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
-        location='부산'
-        price='11,000'
-        isNew={true}
-      />
-      <ProjectCard 
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/kyrielle/71287/ProjectInfo/Cover/1a6c262bb3664008b6475814bac58626.jpg'
-        title='선데이 어드벤쳐 보드게임'
-        location='춘천시'
-        dday='09월 24일(일)'
-        price='5,000'
-        isNew={true}
-      />
-      <ProjectCard 
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
-        title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
-        location='부산'
-        price='11,000'
-        isNew={true}
-        />
-      <ProjectCard
-        image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
-        title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
-        location='부산'
-        price='11,000'
-        isNew={true}
-      />
+      {[...Array(100)].map((e, i) =>
+        <>
+          <ProjectCard key={i+'1st'} 
+            image='https://eventusstorage.blob.core.windows.net/evs/Image/kyrielle/71287/ProjectInfo/Cover/1a6c262bb3664008b6475814bac58626.jpg'
+            title='선데이 어드벤쳐 보드게임'
+            location='춘천시'
+            dday='09월 24일(일)'
+            price='5,000'
+            isNew={true}
+          />
+          <ProjectCard key={i+'2nd'}
+            image='https://eventusstorage.blob.core.windows.net/evs/Image/greentea71/71356/ProjectInfo/Cover/7a96c12df916464891b9de7b4e8cf35b.jpg'
+            title='부산 세븐브릿지 을숙도대교 아웃도어미션게임<새이마이네임>'
+            location='부산'
+            price='11,000'
+            isNew={true}
+          />
+        </>
+      )}
     </div>
   );
 }
