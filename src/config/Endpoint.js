609c500e89f71d65fa52af7c89ed81a02bef07de
jwk <nakazjk@naver.com>
2023-10-17 16:51:29 +0900

    / 제거

--- src/config/Endpoint.js
+++ src/config/Endpoint.js
@@ -2,7 +2,7 @@ function Endpoint() {
   if (process.env.NODE_ENV === 'development') {
     return 'http://localhost:5000';
   } else if (process.env.NODE_ENV === 'production') {
-    return 'https://port-0-final-project-server1-euegqv2blntuic8i.sel5.cloudtype.app/';
+    return 'https://port-0-final-project-server1-euegqv2blntuic8i.sel5.cloudtype.app';
   }
 }
 
