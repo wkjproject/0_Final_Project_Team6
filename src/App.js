4bd73b62477662c7b8e082e1b50d2e29601aaa31
nib <139529654+whiteNib@users.noreply.github.com>
2023-10-21 16:28:05 +0900

    auth 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import './components/ProjectData2.css';
+import "./App.css";
+import "./components/ProjectData2.css";
 //import ProjectData from './components/ProjectData';
-import ProjectData2 from './components/ProjectData2';
-import RewardSelect from './components/RewardSelect';
+import ProjectData2 from "./components/ProjectData2";
+import RewardSelect from "./components/RewardSelect";
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
@@ -17,8 +17,10 @@ import LandingPage from './components/LandingPage/LandingPage';
 import { Login } from './components/Pages/MemberService/Login';
 import IdpwFind from './components/Pages/MemberService/IdpwFind';
 import ManageProj from './components/Pages/ManageProj';
-import ComingProj from './components/Pages/ComingProj';
-import WaitingProj from './components/Pages/WaitingProj';
+
+import ComingProj from './components/Pages/ComingProject/ComingProj';
+import WaitingProj from './components/Pages/WaitingProject/WaitingProj';
+import CreateProj from "./components/Pages/MemberService/CreateProj/CreateProj";
 
 import ReduxTest from './components/Pages/ReduxTest';
 import Auth from './components/HigherOrderComponents/Auth';
@@ -34,13 +36,15 @@ function App() {
   const AuthSignup = Auth(Signup, false);
   const AuthHome = Auth(Home, null);
   const AuthMypage = Auth(Mypage, true);
+  const AuthProjectPay = Auth(ProjectPay, true);
   return (
-    <div className='App'>
+    <div className="App">
       {/* <ProjectData></ProjectData> */}
 
       <BrowserRouter>
         <Header />
         <Routes>
+
           <Route path='/' element={<LandingPage />} />
           <Route path='/home' element={<AuthHome />} />
           <Route path='/openProj' element={<OpenProj />} />
@@ -52,13 +56,15 @@ function App() {
           <Route path='/idpwFind' element={<IdpwFind />} />
           <Route path='/*' element={<NotFound />} />
           <Route path='/project2' element={<ProjectData2 />} />
-          <Route path='/projectPay' element={<ProjectPay />} />
+          <Route path='/projectPay' element={<AuthProjectPay />} />
           <Route path='/reward' element={<RewardSelect />} />
           <Route path='/reduxTest' element={<ReduxTest />} />
           <Route path='/manageProj' element={<ManageProj />} />
           <Route path='/comingProj' element={<ComingProj />} />
           <Route path='/waitingProj' element={<WaitingProj />} />
           <Route path='/mypage' element={<AuthMypage />} />
+          <Route path="/createProj" element={<CreateProj />} />
+
         </Routes>
       </BrowserRouter>
       {/* <hr /> */}
--- src/components/Pages/ComingProj.jsx
+++ src/components/Pages/ComingProj.jsx
@@ -5,3 +5,4 @@ export default function ComingProj() {
 		<div>ComingProj</div>
 	)
 }
+
