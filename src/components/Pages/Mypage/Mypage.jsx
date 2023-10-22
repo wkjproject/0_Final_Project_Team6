ca646f3d7b279c92e70a1c4701324cb22263af9c
jwk <nakazjk@naver.com>
2023-10-22 09:40:36 +0900

    오류 수정 완료

--- src/components/Pages/Mypage/Mypage.jsx
+++ src/components/Pages/Mypage/Mypage.jsx
@@ -1,21 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import '../../../css/ManageProj.css';
-import ProjectCard from '../ProjectCard';
-import Endpoint from '../../../config/Endpoint';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import '../../../css/Mypage/Mypage.css';
+import FundingProject from './FundingProject';
+import MadeProject from './MadeProject';
+import LikeProject from './LikeProject';
+import UserProfileModify from './UserProfileModify';
 
 export default function Mypage() {
   // 리덕스의 userId 가져오기
   const user_id = useSelector((state) => state.auth.auth.userId)
   // 펀딩프로젝트는 fundings 컬렉션에서 user_id가 현재 리덕스 userId랑 일치하는것만 가져와서 project_id 와 projects 컬렉션의 proj_id가 일치하는것만 뿌림
   // 펀딩프로젝트에서 필요한 데이터는 projects 의 projName, projAddr, projPlace // fundings 의 rewards 갯수로 map 돌려서 반복
-  const [fundings, setFundings] = useState();
-  const [fundingProject, setFundingProject] = useState();
+
   
   // 데이터 불러올때까지 mount 값 false
-  const [mount, setMount] = useState(false);
+  const [currTab, setCurrTab] = useState('funding');
+
+    /* --- 탭 변경 함수 --- */
+  const handleTabClick = (tabName) => {
+    setCurrTab(tabName);
+    /* setCurrPage(1); // 탭 변경시, 1페이지로 초기화 */
+  };
 
   // 출발일 하루 전 결제취소누르면 정말 취소하냐고 한번 더 물어본 뒤 거기서 결제취소 누르면 취소되고 fundings 컬렉션에서 데이터 삭제
 
@@ -24,26 +29,7 @@ export default function Mypage() {
 
   // 관심프로젝트는 userProjects 컬렉션에서 users_id가 현재 리덕스 userId랑 일치하는것만 가져와서 userLikeProject 와 projects 컬렉션의 proj_id가 일치하는것만 뿌림
   // 관심프로젝트에서 필요한 데이터는 projects 의 projName, projAddr, projReward[0]
-  useEffect(() => {
-    const endpoint = Endpoint();
-    const fundingProjectData = async () => {
-      try {
-        await axios.post(`${endpoint}/fundingProject`, {
-          user_id
-        }).then((res) => {
-          setFundings(res.data.fundings);
-          setFundingProject(res.data.projects);
-          setMount(true);
-        })
-      } catch (err) {
-        console.log(err);
-      }
-    }
 
-    fundingProjectData();
-  }, []);
-  console.log(fundings)
-  console.log(fundingProject);
   return (
     <div>
       <h1>홍길동</h1>
@@ -52,18 +38,26 @@ export default function Mypage() {
         {/* 승인 대기 프로젝트 탭 */}
         <ul>
           <li
+            className={`tab ${currTab === 'funding' ? 'active' : ''}`}
+            onClick={() => handleTabClick('funding')}
           >
             펀딩 프로젝트
           </li>
           <li
+            className={`tab ${currTab === 'made' ? 'active' : ''}`}
+            onClick={() => handleTabClick('made')}
           >
             제작 프로젝트
           </li>
           <li
+            className={`tab ${currTab === 'like' ? 'active' : ''}`}
+            onClick={() => handleTabClick('like')}
           >
             관심 프로젝트
           </li>
           <li
+            className={`tab ${currTab === 'userProfileModify' ? 'active' : ''}`}
+            onClick={() => handleTabClick('userProfileModify')}
           >
             회원정보 수정
           </li>
@@ -72,24 +66,14 @@ export default function Mypage() {
 
       {/* 선택된 탭에 따라 내용을 표시 */}
       
-      <div className='project-container'>
-        {mount && fundingProject.map((projectArray, index) => {
-          const funding = fundings[index]
-          return projectArray.map((proj) => (
-          <ProjectCard
-            key={proj.projName}
-            projId={proj.proj_id}
-            image={proj.projMainImgPath}
-            MypageImageClass={'fundingProjectImg'}
-            MypageDivClass={funding.fundingStatus === 3 ? 'fundingProjectImgX' : 'fundingProjectText'}
-            MypageDivContent={funding.fundingStatus === 0 ? '대기' : funding.fundingStatus === 1 ? '확정' : ''}
-            title={proj.projName}
-            location={proj.projAddr.split(' ', 2)[1]}
-            dday={proj.projDate}
-            price={proj.projReward[0].projRewardAmount}
-          />
-          ))
-        })}
+      <div className='MypageContainer'>
+        <div className= 'MypageProjectList'>
+        {currTab === 'funding' ? (<FundingProject />):(
+          currTab === 'made' ? (<MadeProject />) : (
+            currTab === 'like' ? (<LikeProject />) : (<UserProfileModify />)
+          )
+        )}
+        </div>
       </div>
 
     </div>
