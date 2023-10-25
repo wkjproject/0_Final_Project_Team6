4b9b71de476aaa4e8e23feff7064e69162f67791
aqubi <msp827@naver.com>
2023-10-25 14:03:58 +0900

    Edit LandingPage

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-
 import './LandingPage.css';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPerson } from "@fortawesome/free-solid-svg-icons";
 
 const Background = styled.div`
-  background-image: url('./img/landing_bg.png');
-  background-size: cover;
-  background-repeat: no-repeat; /* 배경 이미지 반복 금지 */
-
-	border-top: 0.5px solid #cecccd;
+  // background-image: url('./img/landing_bg.png');
+  // background-size: cover;
+  // background-repeat: no-repeat; /* 배경 이미지 반복 금지 */
   height: 100vh;
   width: 100vw;
 `;
@@ -17,20 +16,75 @@ const Background = styled.div`
 export default function LandingPage() {
   const navigate = useNavigate();
 
+  const serviceDesc = [
+    {
+      image: "./img/users-solid.png",
+      title: "혼자하기",
+      text: "최소 필요 인원 및 비용 문제 발생",
+    },
+    {
+      image: "./img/comments-dollar-solid.png",
+      title: "모금하기",
+      text: "이웃과 함께 같은 목적으로  모금",
+    },
+    {
+      image: "./img/people-line-solid.png",
+      title: "함께하기",
+      text: "저렴한 가격으로 group 구성 성공!",
+    }
+  ];
+
   return (
     <Background>
       <div className='landing-container'>
-        <div className='text-section'>
-          <p className='primary-heading'>
-            <b>W</b>HO WANT? <br />
-            <b>W</b>E WANT!
-          </p>
-          <p className='primary-text'>뭔가...   문구.....</p>
+        {/* 1. Intro */}
+        <div className='intro'>
+          <div className='intro-text-section'>
+            <p className='primary-heading'>
+              <b>W</b>HO WANT? <br />
+              <b>W</b>E WANT!
+            </p>
+            <p className='primary-text'>뭔가...   문구.....</p>
+          </div>
+          <div className='intro-image-section'>
+            <img src='./img/landing_image.png' alt='자전거 탄 소년' />
+            <button className='go-button' onClick={()=> navigate('/home')}>함께하기</button>
+          </div>
         </div>
-        <div className='image-section'>
-          <img src='./img/landing_image.png' alt='자전거 탄 소년' />
-          <button className='button' onClick={()=> navigate('/home')}>함께하기</button>
+
+        {/* 2. About Us */}
+        <div className='about'>
+          <div className='about-image-section'>
+          <img src='./img/logo.png' alt='랜딩로고' />
+          </div>
+          <div className='about-text-section'>
+            <p className='sub-text'>W & W란?</p>
+            <h1 className='primary-heading'>WW서비스는 후원과 펀딩 <br /> 그 어딘가...입니다.</h1>
+            <p className='primary-text'>혼자 꿈꾸던 취미를 이웃과 이뤄가세요.<br />추가로 설명이 더 필요합니다.</p>
+            <div className='about-button'>
+              <button className='about-button' onClick={()=> navigate('/home')}>더 알아보기</button>
+            </div>
+          </div>
         </div>
+        {/* 3. Service DESC */}
+        <div className='service'>
+            <div className='service-section-top'>
+              <p className='sub-text'>서비스 소개</p>
+              <h1 className='primary-heading'>펀딩은 이렇게 진행되요.</h1>
+              <p className='primary-text'>이렇게 해서 저렇게 <br /> Lorem Ipsum is simply dummy text<br />  of the printing and typesettingm</p>
+            </div>
+            <div className='service-section-bottom'>
+              {serviceDesc.map((data) => (
+                <div className='service-info'>
+                  <div className='service-info-img'>
+                    <img src={data.image} alt="서비스 이미지" width={'50px'} height={'50px'} />
+                  </div>
+                  <h2>{data.title}</h2>
+                  <p>{data.text}</p>
+                </div>
+              ))}
+            </div>
+          </div>
       </div>
     </Background>
   );
