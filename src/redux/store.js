bf670e739a98a78d162a8f925bf6b368ea8ad9b4
jwk <nakazjk@naver.com>
2023-11-01 13:34:23 +0900

    redux내용을 로컬스토리지에 저장

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,6 +12,9 @@ import { userMailReducer } from './reducer/userMailReducer';
 import { projPlaceAddrReducer } from './reducer/projPlaceAddrReducer';
 import { idReducer } from './reducer/idReducer';
 import { userDataReducer } from './reducer/userDataReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // 저장 엔진을 선택할 수 있습니다.
+
 // 일단은 이렇게 변수 하나당 한개의 리듀서로 가다가
 // 최종안 나오면 user끼리 project끼리 합치는게 나을거같아요.
 
@@ -28,7 +31,16 @@ const rootReducer = combineReducers({
   userData: userDataReducer,
 });
 
+const persistConfig = {
+  key: 'root', // 로컬 스토리지의 키
+  storage, // 저장 엔진
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk, promiseMiddleware))
 );
+
+export const persistor = persistStore(store);
