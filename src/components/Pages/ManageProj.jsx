d084655b74e61e33410aa86c0f5f0c180a397b0b
jwk <nakazjk@naver.com>
2023-11-01 11:25:37 +0900

    빈데이터시 오류수정

--- src/components/Pages/ManageProj.jsx
+++ src/components/Pages/ManageProj.jsx
@@ -19,7 +19,8 @@ export default function ManageProj() {
   /* const ProjectData = useFetch(
     'https://json-server-vercel-sepia-omega.vercel.app/projects'
   ); */
-  const projList = Object.values(ProjectData); // 프로젝트 값을 배열로
+
+  const projList = ProjectData ? Object.values(ProjectData) : [];
 
   const projectPerPage = 6; // 페이지 당 표시할 프로젝트 수(※일단 2개로)
   const [currTab, setCurrTab] = useState('waiting'); // 기본 값: 승인대기 프로젝트
