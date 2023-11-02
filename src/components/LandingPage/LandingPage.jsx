d358cfae052da0ab20c86aa7c96668ebae4b7f0e
nib <139529654+whiteNib@users.noreply.github.com>
2023-11-02 15:12:40 +0900

    Revert "Update LandingPage.jsx"
    This reverts commit 2c806f60e743bc3c1183e7082277806059320511.

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import './LandingPage.css';
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faPerson } from "@fortawesome/free-solid-svg-icons";
-import TopBtn from './../TopBtn/TopBtn';
+
 const Background = styled.div`
   height: 100%;
   width: 100%;
