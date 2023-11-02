d323d04d541457f3138a2a2d16daaeaef410e9a6
aqubi <msp827@naver.com>
2023-11-02 15:46:31 +0900

    TopBtn, Footer 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Footer.css';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
-
 function Footer() {
   return (
     <footer>
@@ -10,32 +9,34 @@ function Footer() {
       <div className="footer-container">
         <div className='footer-padding'>
 					<div className='footer-links-container'>
-					<div className="footer-link">
-							<h4>공지사항</h4>
-							<a href="#"><p>문의사항</p></a>
-							<a href="#"><p>게시판</p></a>
-							<a href="#"><p>Q&A</p></a>
-						</div>
 						<div className="footer-link">
-							<h4>서비스 소개</h4>
-							<a href="#"><p>WW란?</p></a>
-							<a href="#"><p>프로그램 제작 가이드</p></a>
-							<a href="#"><p>프로그램 참여 메뉴얼</p></a>
+								<h4>공지사항</h4>
+								<a href="#"><p>문의사항</p></a>
+								<a href="#"><p>게시판</p></a>
+								<a href="#"><p>Q&A</p></a>
+							</div>
+							<div className="footer-link">
+								<h4>서비스 소개</h4>
+								<a href="#"><p>WW란?</p></a>
+								<a href="#"><p>프로그램 제작 가이드</p></a>
+								<a href="#"><p>프로그램 참여 메뉴얼</p></a>
+							</div>
+							<div className="footer-link">
+								<h4>이용약관</h4>
+								<a href="#"><p>서비스 이용약관</p></a>
+								<a href="#"><p>개인정보처리방침</p></a>
+								<a href="#"><p>취소 및 환불약관</p></a>
+							</div>
+							<div className="footer-link">
+								<h4>회사 소개</h4>
+								<a href="#"><p>☎: 012-345-6789</p></a>
+								<a href="#"><p>Email: WW123@google.com </p></a>
+								<a href="#"><p>서울특별시 중구 세종대로 110</p></a>
+							</div>		
+							<div className="footer-link"></div>
+							<div className="footer-link"></div>							
 						</div>
-						<div className="footer-link">
-							<h4>이용약관</h4>
-							<a href="#"><p>서비스 이용약관</p></a>
-							<a href="#"><p>개인정보처리방침</p></a>
-							<a href="#"><p>취소 및 환불약관</p></a>
-						</div>
-						<div className="footer-link">
-							<h4>회사 소개</h4>
-							<a href="#"><p>☎: 012-345-6789</p></a>
-							<a href="#"><p>Email: WW123@google.com </p></a>
-							<a href="#"><p>서울특별시 중구 세종대로 110</p></a>
-						</div>		
-						<div className="footer-link"></div>
-						<div className="footer-link"></div>							
+					</div>
 					</div>
 
 					<hr></hr>
@@ -53,10 +54,9 @@ function Footer() {
 							</div>
 						</div>
 					</div>
-				</div>
-      </div>
+
+
     </footer>
   );
 }
-
 export default Footer;
