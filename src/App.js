74511fea5000d7227e6f79314ba22dbc1dc15f07
nib <139529654+whiteNib@users.noreply.github.com>
2023-11-03 10:43:25 +0900

    3초로 수정
    3초로 수정

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
       <BrowserRouter>
         <ToastContainer
           position='top-center'
-          autoClose={5000}
+          autoClose={3000}
           hideProgressBar={false}
           newestOnTop={false}
           closeOnClick
